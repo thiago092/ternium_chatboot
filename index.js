9e11546ac6233a6765df1d7efef1ab6827e8c255
Unknown <unknown@example.com>
2020-08-05 15:02:28 -0300

    init project

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express(); // here I use the express() method, instead of the createServer()
+
+
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+});
+
+
+const PORT = process.env.PORT ||  5000;
+app.listen(PORT);
