8ae1cfeb7415a23422e5bf0f72d5504d45adef45
thiago <thiago.augustocarvalho@gmail.com>
2020-08-05 20:43:10 -0300

    heroku enviroments

--- chatbot/chatbot.js
+++ chatbot/chatbot.js
@@ -1,22 +1,17 @@
 'use strict';
 const dialogflow = require('dialogflow');
+const structjson = require('./structjson.js');
 const config = require('../config/keys');
 
 const projectId = config.googleProjectID;
 const sessionId = config.dialogFlowSessionID;
 const languageCode = config.dialogFlowSessionLanguageCode;
-
 const credentials = {
     client_email: config.googleClientEmail,
-    private_key:
-    config.googlePrivateKey,
+    private_key:config.private_key,
 };
-
-
 const sessionClient = new dialogflow.SessionsClient({projectId, credentials});
 const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-
 module.exports = {
     textQuery: async function(text, parameters = {}) {
         let self = module.exports;
@@ -34,21 +29,33 @@ module.exports = {
                 }
             }
         };
+        let responses = await sessionClient.detectIntent(request);
+        responses = await self.handleAction(responses);
+        return responses;
+
+    },
+
+    eventQuery: async function(event, parameters = {}) {
+        let self = module.exports;
+        const request = {
+            session: sessionPath,
+            queryInput: {
+                event: {
+                    name: event,
+                    parameters: structjson.jsonToStructProto(parameters), //Dialogflow's v2 API uses gRPC. You'll need a jsonToStructProto method to convert your JavaScript object to a proto struct.
+                    languageCode: languageCode,
+                },
+            }
+        };
 
         let responses = await sessionClient.detectIntent(request);
         responses = await self.handleAction(responses);
         return responses;
 
-
-
     },
-
-
 
 
     handleAction: function(responses){
         return responses;
     },
-
-
-} 
+}
--- config/keys.js
+++ config/keys.js
@@ -2,7 +2,9 @@ module.exports = {
 
     googleProjectID: 'agent-teste-ternium-jharcy',
     dialogFlowSessionID: 'react-bot-session',
-    dialogFlowSessionLanguageCode:'pt-BR'
-    
+    dialogFlowSessionLanguageCode:'pt-BR',
+    googleClientEmail: 'dialogflow-client@agent-teste-ternium-jharcy.iam.gserviceaccount.com',
+    private_key: '-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCrnUecR1U71ri7\nbdiyhs9GRhm48s1JZJ5gKJgBCSiK21Ec/v+a94PD48ndif2saskDoxs9DMUozL81\nbFSExpOwh5egcb5I1HnA+Zf+5aMwc5vuume0gom5K4/aotji6pgiG0Z+i7Ina/Un\nVyU5SYbvGJFXYXDOJMulPYsU6M6Ar4qZ0A2gbTXTY/dcPxjI3QbOitIW5+4s3cX2\nCDJvCLwup5yQ029Hs+0s2D4de8ZYrrh4tciqKndR7caHbNahEfaE72pQtaeYyr2d\neI755+iOidvYn0kidQoBt3HFnmNv36UpX3zsKWseYLzON0tXrWve/VYmQZvYXrcv\nixx6CBznAgMBAAECggEAGUEMXt2KbUNZrPnrDR5/Kw65eKOA/SoUOlSkRbwUyMD3\nIfFj05NVQxg8G7X+QVyiLGmuDPZaSc4+MbzkKnkpwQJlwu5qEOqfk3vHV8hwPXrj\nI/wvXaYQtqdH7zsj99ng6F1OKuTLeIyk/L87aD+UiTkleOU1xfLZOv2gVW6K+5mx\nICIQ2bZ+1WIo9ck1uBLveWwYF9IQg+WqXb/yASTrH36eRj1iUanVl/pS/tAnJrr+\nOn5xS4t+auo1LnIu5EjxCIKTWgDMqsd3MECA6RbahxKypgVXb/WCQXtg2DU/otxU\ngiqMPKC/2Rf9OjDkWEDqHV8Ssb4uHO0VuyK/fPbiVQKBgQDUminMqk5cVu7zuW/N\nUuwkVFhMzPAZYqsnXZ+5A1U3yT09n/N2lqVwmzsX6lCuMzy0zFC3xEO9Cij6ofUl\nZR6Hw9pdtqPAdHHeaIu9ohw3k+IvnWL/YkyIGTpoTK+eIPfK0h/DUzWLIVLub5Yf\nCu0rDrrMNy7S4KmU+6gTcTLqMwKBgQDOpTzxcbqw+365jTTybDr7hFyx8q5Xsx8N\nufBZXILOJea6tYxNLoCFAZAv9c+lF5Arh8fpA+Sz0bVjpJwLgjN3onk1uwMarjht\nLcryb+ZjOkj8p9IVoHltXgqzc9evf71byjCSFM0N3/lqc4IUX/ig4SVniFltpaqY\nfhn+Eyc2fQKBgQCp8Y2GJ4C5+VfLBSkkpKtUowNuaMdoU2Hi9sjtWIevLzK/5vLz\nkZoveDZ1eH0DsrtM/qFrM/y9yLhXGE1glgMgnWMZVBxzg+Fd2o4RiNsrZhAA5bsz\nz7zDcf8pTlt3eHz66JoxRXtgesPTCumiaNaKe5zAD0BjmuiTydJhsDP11wKBgFET\ncarFVVs1AU5pwZlPr4CZOQk0TwTnqUpoZo2ZB1U8d59PtaFemlwU9e5rQjHi4dTC\nbEPXHDcVE1bp8bNuPSU2INpc6mXgbxEfA15FWx4ZcyN18K49jQbE++vWiow30SKf\nXJkkAiPseV4zvZii4ti0HUnWrEeh1SIJb8dI2ny9AoGANmIquc3pPV39N2Qi06KO\nr2yNUYtKHpZLQ3laEZlyQsghIEjwrIG3nDVe2KrYZ4YanD5xvRuV0EgsenxQweaF\naSJ6qN6jGtwlfz/8tqbMjc7fpbXfLrX9t7CBOGHcISbCGyUMBBe9V0IYeLj+6E9y\ne6s2YcXDLhsyR3pJoN4KNiU=\n-----END PRIVATE KEY-----\n'
+  
 }
 
